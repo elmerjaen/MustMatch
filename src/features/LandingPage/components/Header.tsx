da576a95c65020a01d97d1755f82d343a966df38
Alois Carrera <aloiscarrera@gmail.com>
2021-11-19 22:16:50 +0100

    feat: :dizzy: Añade animación y cuadro de información

--- src/features/LandingPage/components/Header.tsx
+++ src/features/LandingPage/components/Header.tsx
@@ -1,23 +1,48 @@
-const Header = () => (
-  <header className="top-0 left-0 right-0 absolute z-10 flex justify-between py-3 px-10 bg-black bg-opacity-30 backdrop-filter backdrop-blur-sm text-white">
-    <div className="flex items-center gap-3">
-      <div className="bg-white rounded-full inline-block p-1">
-        <img src="src/images/utp-logo.png" width="50px" height="50px" alt="" />
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+const Header = () => {
+  const [infoOpened, setInfoOpened] = useState(false);
+
+  return (
+    <header className="top-0 left-0 right-0 absolute z-10 flex justify-between py-3 px-10 bg-black bg-opacity-30 backdrop-filter backdrop-blur-sm text-white">
+      <div className="flex items-center gap-3">
+        <div className="bg-white rounded-full inline-block p-1">
+          <img
+            src="src/images/utp-logo.png"
+            width="50px"
+            height="50px"
+            alt=""
+          />
+        </div>
+        <span className="font-medium text-3xl">UTP</span>
       </div>
-      <span className="font-medium text-3xl">UTP</span>
-    </div>
-    <div className="flex items-center gap-10">
-      <button type="button" className="text-xl">
-        Inicio
-      </button>
-      <button
-        type="button"
-        className="border-2 p-3 inline-block transition-colors hover:bg-white hover:text-black hover:border-black"
-      >
-        Más información
-      </button>
-    </div>
-  </header>
-);
+      <div className="relative flex items-center gap-10">
+        <button type="button" className="text-xl">
+          Inicio
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setInfoOpened(!infoOpened);
+          }}
+          className="border-2 p-3 inline-block transition-colors hover:bg-white hover:text-black hover:border-black"
+        >
+          Más información
+        </button>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={infoOpened ? { opacity: 1, top: '5rem' } : undefined}
+          transition={{ duration: 1, ease: 'anticipate' }}
+          className="pointer-events-none absolute w-96 bg-white bg-opacity-30 p-5 rounded-xl shadow-xl right-0 text-justify"
+        >
+          El equipo Must Match tiene la misión de promover la enseñanza del
+          lenguaje de señas en nuestro territorio panameño y fomentar la
+          inclusión social de aquellas personas con necesidades especiales
+        </motion.p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
